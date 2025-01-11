f224430dd35d76289d1c8952f445dd125880d3a7
Dell <shweta.gujar260819@gmail.com>
2025-01-11 11:13:24 +0530

    commiting the code related to printf

---
+++ src/variable.java
@@ -0,0 +1,20 @@
+public class variable {
+    public static void main(String[] args) {
+        //This class is for practising variables
+        int a=9;
+
+        System.out.printf("%d x 1 = %d%n", a, a * 1);
+        System.out.printf("%d x 2 = %d%n", a, a * 2);
+        System.out.printf("%d x 3 = %d%n", a, a * 3);
+        System.out.printf("%d x 4 = %d%n", a, a * 4);
+        System.out.printf("%d x 5 = %d%n", a, a * 5);
+        System.out.printf("%d x 6 = %d%n", a, a * 6);
+        System.out.printf("%d x 7 = %d%n", a, a * 7);
+        System.out.printf("%d x 8 = %d%n", a, a * 8);
+        System.out.printf("%d x 9 = %d%n", a, a * 9);
+        System.out.printf("%d x 10 = %d%n", a, a * 10);
+
+    }
+
+    }
+
